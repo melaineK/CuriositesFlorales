ae59c63abbc2e31b78030e1bf899ae14d7348f2d
melaineK <koffiamelaine@gmail.com>
2015-05-15 18:45:44 +0200

    class product

---
+++ CuriositesFlorales/src/com/cf/domains/Product.java
@@ -0,0 +1,28 @@
+package com.cf.domains;
+
+public class Product {
+	
+	private long id;
+	private String reference;
+	private float  price;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getReference() {
+		return reference;
+	}
+	public void setReference(String reference) {
+		this.reference = reference;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+
+}
