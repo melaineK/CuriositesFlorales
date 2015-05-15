1e069872202171f30d020e5f4a133ba793792f18
melaineK <koffiamelaine@gmail.com>
2015-05-15 17:03:10 +0200

    classe client

--- CuriositesFlorales/src/com/cf/domains/Customer.java
+++ CuriositesFlorales/src/com/cf/domains/Customer.java
@@ -6,8 +6,37 @@
 package com.cf.domains;
 
 public class Customer {
+	
+	private long id;
 	private String name;
 	private String firstName;
 	private String address;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	
+	
 
 }
