bc27ac9126d94f00393dc35d12e7637cd4846c06
melaineK <koffiamelaine@gmail.com>
2015-05-15 17:08:56 +0200

    page d'index avec le main principal

---
+++ CuriositesFlorales/src/com/cf/view/Index.java
@@ -0,0 +1,18 @@
+/*
+ * Page d'acceuil
+ */
+package com.cf.view;
+
+import javax.swing.JFrame;
+
+public class Index {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Main indexFrame = new Main();		
+		
+		indexFrame.setVisible(true);
+
+	}
+
+}
