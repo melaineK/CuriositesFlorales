a9e1bf43cc361b6b0224a182c240c4b79c59e57b
melaineK <koffiamelaine@gmail.com>
2015-05-15 15:37:40 +0200

    classe customer

---
+++ CuriositesFlorales/src/com/cf/domains/Customer.java
@@ -0,0 +1,13 @@
+/*
+ * Gere les clients
+ * liste de tous les clients
+ * fidélisation des clients
+ */
+package com.cf.domains;
+
+public class Customer {
+	private String name;
+	private String firstName;
+	private String address;
+
+}
